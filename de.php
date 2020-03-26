0b2352affa5198fc27e56fcb17a6abf05b295337
Simon <meet@simon-frey.eu>
2020-03-26 15:42:15 +0100

    init

---
+++ de.php
@@ -0,0 +1,108 @@
+<?php
+// 
+// global
+$globalLang = array(
+    "slogan"=>"Binge Together - Filme zusammen mit Ihren Freunden anschauen",
+    "description"=>"Genießen Sie einen Filmabend mit Ihren Freunden
+    Sich nah fühlen, auch wenn sie meilenweit voneinander entfernt sind
+    ⏰ Starten Sie Ihren Film genau zur gleichen Zeit
+    📞 Sprechen Sie über Ihre Emotionen per Voice-Chat
+    👍 Kein Browser-Plugin erforderlich!
+    🤩 Es ist kostenlos.
+    Binge Together als bessere Alternative zur Netflix Party nutzen",
+
+    "sponsoringTitle"=>"Sponsoring",
+    "sponsoringExplanation"=>"Binge Together wird durch Anzeigen unterstützt.",
+    "sponsoringLink"=>"Unterstützung durch den Kauf dieser Werbefläche.",
+
+    "supportPapPal"=>"Unterstützung über PayPal",
+    "feedback"=>"Feedback",
+    "imprint"=>"Impressum & Datenschutz",
+    "helpTranslating"=>"Übersetzen helfen"
+
+);
+
+//
+// index
+$indexLang = array(
+    "joinTitle"=>"An einer Filmparty teilnehmen",
+    "joinSubtitle"=>"Geben Sie die ID ein, die Ihr Freund Ihnen gegeben hat",
+    "joinButtonText"=>"Teilnehmen",
+
+
+    "createTitle"=>"Erstellen Sie eine neue Filmparty",
+    "createSubtitle"=>"Geben Sie die Url des Videos ein, das Sie mit Ihren Freunden ansehen möchten",
+    "createButtonText"=>"Erstellen",
+
+
+    "explanationTitle"=>"Genießen Sie einen Filmabend mit Ihren Freunden",
+    "explanationSubtitle"=>"Sich nah fühlen, auch wenn sie meilenweit voneinander entfernt sind",
+    "explanationBenefitsList"=>"<li>⏰ Beginnen Sie Ihren Film zur <b>genauen gleichen Zeit</b></li>
+    <li>📞 Sprechen Sie über Ihre Emotionen über <b>Sprachchat</b></li>
+    <li>👍 Kein Browser-Plugin erforderlich!</li>
+    <li>🤩 Es ist <b>frei</b></li>",
+    "explanationHowToTitle"=>"How to",
+    "explanationHowToList"=>"<li>1. Erstellen Sie eine neue Filmparty, indem Sie <b>die url</b> des Films, den Sie sich ansehen möchten, eingeben >/li>.
+    <li>2. <b>Senden Sie den Parteicode</b> an Ihre Freunde</li>
+    <li>3. stellen Sie sicher, dass jeder <b>ein Konto hat und bei dem von Ihnen gewählten Streaming-Dienst angemeldet ist!</li>
+    <li>4. <b>Öffnen Sie den Voice-Chat</b> in einem neuen Tabulator</li>
+    <li>5. Warten Sie, bis der Countdown auf Null steht und <b>Ihr Filmabend beginnt</b></li>",
+
+
+    "supportedServicesTitle"=>"Unterstützte Dienstleistungen",
+    "supportedServicesSubtitle"=>"Diese Streaming-Dienste werden derzeit von Binge Together unterstützt. Weitere werden bald folgen...",
+    "supportedServicesExplanation"=>'Binge Together wird in der Lage sein, alle online verfügbaren Streaming-Sites zu unterstützen, da es direkt in
+    Ihr Browser. Für den aufgelisteten haben wir herausgefunden, wie man das Autoplay aktiviert. Wenn Sie Informationen haben
+    über andere Streaming-Dienste, zögern Sie nicht, <a href="mailto:bingetogether@simon-frey.com">Kontakt aufzunehmen </a>',
+
+
+
+    "netflixPartyAlernativeTitle"=>"Binge Together als Netflix-Party-Alternative",
+    "netflixPartyAlernativeSubtitle"=>"Wie unterscheidet sich Binge Together von Netflix Party?",
+    "netflixPartyAlernativeExplanation"=>'<li>👍 Kein Bedarf für ein Browser-Plugin</li>
+    <li>🗣️ Unterstützung für den Sprachchat</li>
+    <li>😇 Funktioniert in allen Browsern (nicht nur in Chrome)</li>
+    <li>🎥 Unterstützt mehr Streaming-Dienste als Netflix</li>',
+
+    
+
+);
+
+//
+// party
+$partyLang = array(
+    "ID"=>"ID",
+    "copiedURL"=>"Film-Party-Url kopiert",
+
+    "countdownTitle"=>"Countdown zur Filmparty",
+    "countdownSubtitle"=>"Ihre Filmparty beginnt, wenn der Countdown auf Null steht.",
+    "watchVia"=>"Sie werden zuschauen über",
+    "preperation"=>"Zur Vorbereitung <b>Öffnen Sie bitte den Film und prüfen Sie, ob er automatisch gestartet wird</b>",
+    "openMovie"=>"Film öffnen",
+
+    "alreadyStartedTitle"=>"Filmparty hat bereits begonnen",
+    "alreadyStartedSubitle"=>"Diese Filmparty hat bereits begonnen",
+    "alreadyStartedDescription"=>"Die Filmparty läuft schon eine Weile:",
+    "joinParty"=>"An Filmparty teilnehmen",
+
+    "firefoxTitle"=>"Sie verwenden Firefox",
+    "firefoxSubitle"=>"Firefox blockiert die automatische Wiedergabe",
+    "firefoxdDescription"=>"Das folgende Video erklärt, wie man sie aktiviert. Es verwendet Netflix als Beispiel, aber dies funktioniert mit allen Websites",
+
+
+    "voiceChatTitle"=>"Sprachchat öffnen",
+    "voiceChatSubitle"=>"Ihr Voice-Chat wird über Jitsi Meet gehostet.",
+    "voiceChatButton"=>"Sprachchat öffnen",
+);
+
+//
+// errors
+$errorsLang = array(
+    "error"=>"Fehler",
+    "noMovieURL"=>"Film-URL ist nicht gesetzt",
+    "dateNotSet"=>"Datum ist nicht festgelegt",
+    "timeNotSet"=>"Die Startzeit ist nicht festgelegt",
+    "dbError"=>"DB Fehler",
+    "noPartyID"=>"Keine Party-ID eingestellt",
+    "partyNotFound"=>"Party wurde nicht gefunden. Bitte prüfen Sie, ob die Party-ID korrekt war."
+);
