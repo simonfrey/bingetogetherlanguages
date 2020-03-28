f796a22ae9bf16cc99b07ce130bf0d6e17f4bed6
Simon <meet@simon-frey.eu>
2020-03-28 10:58:23 +0100

    Add hindi

---
+++ hin.php
@@ -0,0 +1,108 @@
+<?php
+// 
+// global
+$globalLang = array(
+    "slogan"=>"Binge Together - watch movies together with your friends",
+    "description"=>"Enjoy a movie night with your friends
+    Feel close, even when being miles apart
+    ⏰ Start you movie at the exact same time
+    📞 Talk about your emotions via voice chat
+    👍 No browser plugin required!
+    🤩 It's free
+    Use Binge Together as better Netflix Party alternative",
+
+    "sponsoringTitle"=>"Sponsoring",
+    "sponsoringExplanation"=>"Binge Together is supported by ads.",
+    "sponsoringLink"=>"Support by purchasing this ad space.",
+
+    "supportPapPal"=>"Support via PayPal",
+    "feedback"=>"Feedback",
+    "imprint"=>"Imprint & Privacy",
+    "helpTranslating"=>"Help translating"
+
+);
+
+//
+// index
+$indexLang = array(
+    "joinTitle"=>"Join a movie party",
+    "joinSubtitle"=>"Enter the ID your friend gave you",
+    "joinButtonText"=>"Join",
+
+
+    "createTitle"=>"Create a new movie party",
+    "createSubtitle"=>"Enter the url of the video you want to watch with your friends",
+    "createButtonText"=>"Create",
+
+
+    "explanationTitle"=>"Enjoy a movie night with your friends",
+    "explanationSubtitle"=>"Feel close, even when being miles apart",
+    "explanationBenefitsList"=>"<li>⏰ Start you movie at the <b>exact same time</b></li>
+    <li>📞 Talk about your emotions via <b>voice chat</b></li>
+    <li>👍 No browser plugin required!</li>
+    <li>🤩 It's <b>free</b></li>",
+    "explanationHowToTitle"=>"How to",
+    "explanationHowToList"=>"<li>1. Create a new movie party by <b>entering the url</b> of the movie you want to watch</li>
+    <li>2. <b>Send the party code</b> to your friends</li>
+    <li>3. Make sure everyone <b>has an account & is signed in</b> into the streaming service you choose!</li>
+    <li>4. <b>Open the voice chat</b> in a new tab</li>
+    <li>5. Wait until the countdown hits zero and <b>your movie night starts</b></li>",
+
+
+    "supportedServicesTitle"=>"Supported services",
+    "supportedServicesSubtitle"=>"This streaming services are currently supported by Binge Together. More are coming soon...",
+    "supportedServicesExplanation"=>'Binge Together will be able to support all streaming sites available online, as it works directly in
+    your browser. For the listed one we figured out how to enable the autoplay. If you have information
+    about other streaming services, do not hesitate to <a href="mailto:bingetogether@simon-frey.com">get in contact </a>',
+
+
+
+    "netflixPartyAlernativeTitle"=>"Binge Together as Netflix Party alternative",
+    "netflixPartyAlernativeSubtitle"=>"How does Binge Together differ from Netflix Party?",
+    "netflixPartyAlernativeExplanation"=>' <li>👍 No need for a browser plugin</li>
+    <li>🗣️ Voice chat support</li>
+    <li>😇 Works in all browsers (not only in chrome)</li>
+    <li>🎥 Supports more streaming services apart from Netflix</li>',
+
+    
+
+);
+
+//
+// party
+$partyLang = array(
+    "ID"=>"ID",
+    "copiedURL"=>"Copied party url",
+
+    "countdownTitle"=>"Countdown to movie party",
+    "countdownSubtitle"=>"Your movie party starts when the countdown hits zero",
+    "watchVia"=>"You are going to watch via",
+    "preperation"=>"As preperation please <b>open the movie and check if it autostarts</b>",
+    "openMovie"=>"Open Movie",
+
+    "alreadyStartedTitle"=>"movie party already started",
+    "alreadyStartedSubitle"=>"This movie party has already started",
+    "alreadyStartedDescription"=>"The movie party already runs for a bit:",
+    "joinParty"=>"Join movie party",
+
+    "firefoxTitle"=>"You are using Firefox",
+    "firefoxSubitle"=>"Firefox blocks autoplay",
+    "firefoxdDescription"=>"The following video explains how to enable it. It uses Netflix as example, but this works with all websites",
+
+
+    "voiceChatTitle"=>"Open voice chat",
+    "voiceChatSubitle"=>"Your voice chat is hosted via Jitsi Meet",
+    "voiceChatButton"=>"Open Voice Chat",
+);
+
+//
+// errors
+$errorsLang = array(
+    "error"=>"Error",
+    "noMovieURL"=>"Movie url is not set",
+    "dateNotSet"=>"Date is not set",
+    "timeNotSet"=>"Start time is not set",
+    "dbError"=>"DB Error",
+    "noPartyID"=>"No party id set",
+    "partyNotFound"=>"Party was not found. Please check if the party id was correct"
+);
